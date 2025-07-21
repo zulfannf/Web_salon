de5b4869e9885fa53a3c9c7b8e641d5e32a68976
zam. <znurfadhil19@gmail.com>
2025-07-21 13:30:59 +0700

    update pages

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function loadComponent(id, filePath) {
+    const target = document.getElementById(id);
+    if(target) {
+        fetch(filePath)
+        .then(res => res.text())
+        .then(data => {
+            target.innerHTML = data;
+        })
+        .catch(err => console.error('Gagal load ${filePath}: ', err));
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("navbar", "/components/navbar.html");
+    loadComponent("footer", "/components/footer.html");
+    loadComponent("copyright", "/components/copyright.html");
+});
