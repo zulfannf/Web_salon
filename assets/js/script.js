8a6c2b1623d6d073c090a8a4f07dba618170ba84
zam. <znurfadhil19@gmail.com>
2025-07-21 14:16:18 +0700

    update bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
+function resolvePath(path) {
+  const currentPath = window.location.pathname;
+  if (currentPath.includes("/pages/")) {
+    return `../${path}`;
+  } else {
+    return path;
+  }
+}
+
 function loadComponent(id, filePath) {
     const target = document.getElementById(id);
     if(target) {
-        fetch(filePath)
+        fetch(resolvePath(filePath))
         .then(res => res.text())
         .then(data => {
             target.innerHTML = data;
         })
-        .catch(err => console.error('Gagal load ${filePath}: ', err));
+        .catch(err => console.error(`Gagal load ${filePath}: `, err));
     }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadComponent("navbar", "../components/navbar.html");
-    loadComponent("footer", "../components/footer.html");
-    loadComponent("copyright", "../components/copyright.html");
+    loadComponent("navbar", "components/navbar.html");
+    loadComponent("footer", "components/footer.html");
+    loadComponent("copyright", "components/copyright.html");
 });
